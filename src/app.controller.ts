213b0ffa20d5466458114f398d0a9cf8df1c5a9c
Victor Mafra <victorvmafra97@gmail.com>
2023-08-21 00:49:27 -0300

    feat Post/users/tweets

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,25 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpException, HttpStatus, Post, Res } from '@nestjs/common';
 import { AppService } from './app.service';
+import { CreateUserDto } from './DTOS/user.dto';
+import { response } from 'express';
+import { CreateTweetDto } from './DTOS/tweet.dto';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+
+  @Post('sign-up')
+  @HttpCode(HttpStatus.OK)
+  createUser(@Body() body: CreateUserDto) {
+      return this.appService.createUser(body)
+  }
+
+  @Post('tweets')
+  @HttpCode(HttpStatus.CREATED)
+  createTweet(@Body() body: CreateTweetDto) {
+      return this.appService.createTweet(body)
   }
 }
+
+
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,32 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { CreateUserDto } from './DTOS/user.dto';
+import { User } from './entities/user.entity';
+import { CreateTweetDto } from './DTOS/tweet.dto';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  private tweets = []
+  private users = []
+
+  constructor(){
+    this.tweets= []
+    this.users = []
   }
+
+
+  createUser(body: CreateUserDto){
+    this.users.push(body)
+  }
+
+  createTweet(body: CreateTweetDto){
+    const {username, tweet} = body
+    const userfind = this.users.find((u) => u.username === username);
+    if (!userfind) throw new HttpException("Not Authorized", HttpStatus.UNAUTHORIZED)
+
+    const tweetObj = {username, tweet, avatar: userfind.avatar}
+
+    this.tweets.push(tweetObj)
+  }
+
+
 }
